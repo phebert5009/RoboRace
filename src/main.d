ce6020501131b26b830589fc9df8822469007cd3
= <=>
2017-08-28 13:26:51 -0600

    V 0.0.1

---
+++ src/main.d
@@ -0,0 +1,22 @@
+import dsfml.graphics;
+import grid;
+immutable Color ItemBlank = Color(27,130,95);
+
+int main(string[] args) {
+    auto window = new RenderWindow(VideoMode(40*12+5*7+90*5,40*12+120),"RoboRace");
+    window.setFramerateLimit(30);
+    Grid grid = new Grid();
+    
+    while(window.isOpen) {
+        Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == Event.EventType.Closed) {
+                window.close();
+            }
+        }
+        window.clear(Color(67,175,125));
+        window.draw(grid);
+        window.display();
+    }
+    return 0;
+}
