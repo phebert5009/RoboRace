07fa4d6b6599659f7819ea682358a27d38b4f339
thomas <thryniw.2016@centrenord.ab.ca>
2017-08-30 14:47:21 -0600

    Changed card and deck

--- src/robo_race/card.d
+++ src/robo_race/card.d
@@ -2,11 +2,9 @@ module robo_race.card;
 
 import dsfml.graphics;
 
-//private int factor = 6
-//private int maxCard = 14 * factor;
 //private string ratio = "1:3:3:1:3:2:1" // met ceci dans deck.d, comme fonction pour créer un deck
 
-struct Card (int cardNum){
+struct Card {
   /*Card Attributes:
 	- priority
     - image
@@ -15,36 +13,36 @@ struct Card (int cardNum){
 	- getPriority ()
 	- displayImage (x,y)
 	- getName ()
-	- setName (cardNumber)
   */
   
   
-    private uint priority;
+    private uint priority = cardNum;
     private string name;
     
+	setName (cardNum, factor)
+	
     uint priority() @property {
 	    return _priority;
     }
   
     string name()  @property {
-	    return Card.name;
+	    return _name;
     }
   
-    this(string name, uint priority) {
-        _name = name;
+    this(uint priority, int factor) {
         _priority = priority;
+		setName(priority, factor)
     }
     
-  /* pas besoin
-  void setName (int cardNum) { 
-	if (cardNum <= factor) Card.name = "U-Turn";
-	else if (factor < cardNum && cardNum <= factor * 7) {
-	  if (cardNum % 2 = 1) Card.name = "Left";
-	  else Card.Name = "Right"
+  void setName (int priority, int factor) { 
+	if (priority <= factor) _name = "U-Turn";
+	else if (priority < priority && priority <= factor * 7) {
+	  if (priority % 20 = 1) _name = "Left";
+	  else _Name = "Right"
 	}
-	else if (factor * 7 < cardNum && cardNum <= factor * 8) Card.name = "Back Up"
-	else if (factor * 8 < cardNum && cardNum <= factor * 11) Card.name = "Move 1"
-	else if (factor * 11 < cardNum && cardNum <= factor * 13) Card.name = "Move 2"
-	else if (factor * 13 < cardNum && cardNum <= factor * 14) Card.name = "Move 3" 
-  }*/
+	else if (factor * 7 < priority && priority <= factor * 8) _name = "Back Up"
+	else if (factor * 8 < priority && priority <= factor * 11) _name = "Move 1"
+	else if (factor * 11 < priority && priority <= factor * 13) _name = "Move 2"
+	else if (factor * 13 < priority && priority <= factor * 14) _name = "Move 3" 
+  }
 }
--- src/robo_race/deck.d
+++ src/robo_race/deck.d
@@ -1,11 +1,18 @@
 module robo_race.deck;
 
+//private int factor = 60;
+//private int maxCard = 14 * 10 * factor;
+//private string ratio = "1:3:3:1:3:2:1"
+
 struct Deck {
-    string[] cards = [
-        "left:10",
-        "right:20",
-    ];
-    alias cards this;
+    Card[] cards;
+	
+	this(int factor = 6) {
+	  int maxCard = 14 * factor * 10
+	  for(i = 10; i <= maxCard; i += 10) cards ~= Card(i, factor);
+	}
+	
+    //alias cards this; // pas certain si on a besoin
     void shuffle() {
         import std.random;
         cards.randomShuffle();
