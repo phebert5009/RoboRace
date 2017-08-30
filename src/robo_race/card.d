b8bd63241af02b46c445422d7cdff34025c46ac8
thomas <thryniw.2016@centrenord.ab.ca>
2017-08-30 11:33:50 -0600

    Card updated & player.png added

--- src/robo_race/card.d
+++ src/robo_race/card.d
@@ -1,5 +1,45 @@
 module robo_race.card;
 
-struct Card {
+import dsfml.graphics;
 
+//private int factor = 6
+//private int maxCard = 14 * factor;
+//private string ratio = "1:3:3:1:3:2:1"
+
+struct Card (int cardNum){
+  /*Card Attributes:
+	- priority
+    - image
+    - name
+	Card Methods
+	- getPriority ()
+	- displayImage (x,y)
+	- getName ()
+	- setName (cardNumber)
+  */
+  
+ /* int Card.priority = cardNum * 10;
+  string Card.name;
+  
+  setName(cardNum);
+  
+  int getPriority () {
+	return Card.priority;
+  }
+  
+  string getName () {
+	return Card.name;
+  }
+  
+  void setName (int cardNum) {
+	if (cardNum <= factor) Card.name = "U-Turn";
+	else if (factor < cardNum && cardNum <= factor * 7) {
+	  if (cardNum % 2 = 1) Card.name = "Left";
+	  else Card.Name = "Right"
+	}
+	else if (factor * 7 < cardNum && cardNum <= factor * 8) Card.name = "Back Up"
+	else if (factor * 8 < cardNum && cardNum <= factor * 11) Card.name = "Move 1"
+	else if (factor * 11 < cardNum && cardNum <= factor * 13) Card.name = "Move 2"
+	else if (factor * 13 < cardNum && cardNum <= factor * 14) Card.name = "Move 3" */
+  }
 }
