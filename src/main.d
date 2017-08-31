49017a6d4dcab55f5264b379004fa38e3932d2bb
= <cpierrot273@gmail.com>
2017-08-31 13:05:42 -0600

    found ourselve a font

--- src/main.d
+++ src/main.d
@@ -4,30 +4,31 @@ import std.stdio;
 immutable Color ItemBlank = Color(27,130,95);
 
 int main(string[] args) {
-    auto window = new RenderWindow(VideoMode(40*12+5*7+90*5,40*12+120),"RoboRace");
-    window.setFramerateLimit(30);
-    Grid grid = new Grid();
-    Clock clock = new Clock();
-    Player player = new Player("objects/Player1.png");
-    player.turnLeft();
-    player.move();
-    Deck deck = Deck(600); //600 is not noticeable
-    Hand hand;
-    hand.drawCards(deck);
-    writeln(hand);
-    while(window.isOpen) {
-        Event event;
-        while(window.pollEvent(event)) {
-            if(event.type == Event.EventType.Closed) {
-                window.close();
-            }
-        }
-        window.clear(Color(67,175,125));
-        window.draw(grid);
-        window.draw(player);
-        window.display();
-        write("\r",clock.restart,"            "); // display frame time
-    }
-    writeln();
-    return 0;
+	auto window = new RenderWindow(VideoMode(40*12+5*7+90*5,40*12+120),"RoboRace");
+	window.setFramerateLimit(30);
+	Grid grid = new Grid();
+	Clock clock = new Clock();
+	Player player = new Player("objects/Player1.png");
+	player.turnLeft();
+	player.move();
+	Deck deck = Deck(600); //600 is not noticeable
+	Hand hand;
+
+	hand.drawCards(deck);
+	writeln(hand);
+	while(window.isOpen) {
+		Event event;
+		while(window.pollEvent(event)) {
+			if(event.type == Event.EventType.Closed) {
+				window.close();
+			}
+		}
+		window.clear(Color(67,175,125));
+		window.draw(grid);
+		window.draw(player);
+		window.display();
+		write("\r",clock.restart,"            "); // display frame time
+	}
+	writeln();
+	return 0;
 }
--- src/robo_race/mobile.d
+++ src/robo_race/mobile.d
@@ -3,96 +3,101 @@ module robo_race.mobile;
 import dsfml.graphics;
 
 enum Direction {
-    up,
-    down,
-    right,
-    left
+	up,
+	down,
+	right,
+	left
 }
 
 /// pieces that can move, such as players or goals
 class MobilePiece : Drawable {
-    struct State {
-        float rotation;
-        Vector2f position;
-    }
-    protected Vector2i _position;
-    protected Texture texture;
-    protected State current, target;
-    protected Sprite sprite;
-    
-    this(string imageFile) {
-        texture = new Texture();
-        if(!texture.loadFromFile(imageFile)) throw new Exception("unable to load texture: " ~ imageFile);
-        sprite = new Sprite();
-        sprite.setTexture(texture);
-        auto lob = sprite.getLocalBounds;
-        sprite.origin = Vector2f(lob.width/2,lob.height/2);
-    }
-    
-    Vector2f position() @property {
-        Vector2f ans = Vector2f(0.0f,0.0f);
-        // first center on tile.
-        ans += Vector2f(20,20);
-        // then place on specific tile
-        ans += _position * 40;
-        return ans;
-    }
-    
-    void turnRight() {
-        sprite.rotation = sprite.rotation + 90;
-        sprite.rotation = sprite.rotation % 360;
-    }
-    
-    void turnLeft() {
-        sprite.rotation = sprite.rotation - 90;
-        sprite.rotation = (sprite.rotation + 360) % 360;
-    }
-    
-    void uTurn() {
-        sprite.rotation = sprite.rotation + 180;
-        sprite.rotation = sprite.rotation % 360;
-    }
-    
-    void move(int n = 1) {
-        if(sprite.rotation == 0) {
-            _position += Vector2i(0,n);
-        } else if(sprite.rotation == 90) {
-            _position += Vector2i(-n,0);
-        } else if(sprite.rotation == 180) {
-            _position += Vector2i(0,-n);
-        } else if(sprite.rotation == 270) {
-            _position += Vector2i(n,0);
-        }
-    }
-    
-    void moveLeft(int n = 1) { // for crabLegs
-        if(sprite.rotation == 0) {
-            _position += Vector2i(n,0);
-        } else if(sprite.rotation == 90) {
-            _position += Vector2i(0,-n);
-        } else if(sprite.rotation == 180) {
-            _position += Vector2i(-n,0);
-        } else if(sprite.rotation == 270) {
-            _position += Vector2i(0,n);
-        }
-    }
-    
-    void moveGlobal(Direction direction, int n = 1) {
-        if(direction%2) n = -n;
-        if(direction/2) { //horizontal
-            _position += Vector2i(n,0);
-        } else { //vertical
-            _position += Vector2i(0,n);
-        }
-    }
-    
-    void draw(RenderTarget target, RenderStates states) {
-        sprite.position = position;
-        sprite.draw(target, states);
-    }
+	struct State {
+		float rotation;
+		Vector2f position;
+		float travelPt;
+	}
+	protected Texture texture;
+	protected State current, target;
+	protected Sprite sprite;
+	
+	this(string imageFile) {
+		texture = new Texture();
+		if(!texture.loadFromFile(imageFile)) throw new Exception("unable to load texture: " ~ imageFile);
+		sprite = new Sprite();
+		sprite.setTexture(texture);
+		auto lob = sprite.getLocalBounds;
+		sprite.origin = Vector2f(lob.width/2,lob.height/2);
+	}
+	
+	Vector2f position() @property {
+		Vector2f ans = Vector2f(0.0f,0.0f);
+		// first center on tile.
+		ans += Vector2f(20,20);
+		// then place on specific tile
+		ans += current.position * 40;
+		return ans;
+	}
+	
+	void turnRight() {
+		sprite.rotation = sprite.rotation + 90;
+		sprite.rotation = sprite.rotation % 360;
+	}
+	
+	void turnLeft() {
+		target.rotation = sprite.rotation - 90;
+		sprite.rotation = (sprite.rotation + 360) % 360;
+	}
+	
+	void uTurn() {
+		target.rotation = sprite.rotation + 180;
+		target.rotation = sprite.rotation % 360;
+	}
+	
+	void move(int n = 1) {
+		if(sprite.rotation == 0) {
+			target.position += Vector2i(0,n);
+		} else if(sprite.rotation == 90) {
+			target.position += Vector2i(-n,0);
+		} else if(sprite.rotation == 180) {
+			target.position += Vector2i(0,-n);
+		} else if(sprite.rotation == 270) {
+			target.position += Vector2i(n,0);
+		}
+	}
+	
+	void moveLeft(int n = 1) { // for crabLegs
+		if(sprite.rotation == 0) {
+			target.position += Vector2f(n,0);
+		} else if(sprite.rotation == 90) {
+			target.position += Vector2f(0,-n);
+		} else if(sprite.rotation == 180) {
+			target.position += Vector2f(-n,0);
+		} else if(sprite.rotation == 270) {
+			target.position += Vector2f(0,n);
+		}
+	}
+	
+	void moveGlobal(Direction direction, int n = 1) {
+		if(direction%2) n = -n;
+		if(direction/2) { //horizontal
+			target.position += Vector2f(n,0);
+		} else { //vertical
+			target.position += Vector2f(0,n);
+		}
+	}
+
+	void update() {
+		
+	}
+
+	void draw(RenderTarget target, RenderStates states) {
+		update();
+		sprite.position = position;
+		sprite.draw(target, states);
+	}
 }
 
 struct MoveInfo {
-    MobilePiece movingPiece;
-    Direction travelDir;
+	MobilePiece movingPiece;
+	Direction travelDir;
 }
