86cc3b94401f627fc12f56fcaf04ee17eb661f3b
thomas <thryniw.2016@centrenord.ab.ca>
2017-08-30 19:52:11 -0600

    Alphabetized the options list

--- src/robo_race/hand.d
+++ src/robo_race/hand.d
@@ -1,8 +1,17 @@
 module robo_race.hand;
 
 import robo_race;
+import robo_race.deck
 import std.algorithm : sort;
 
 struct Hand {
     Card[] cards;
+	int size = 9
+	
+	void drawCards () {
+		for (i = 0; i < size; i++) {
+			cards ~= deck.cards[0]
+			
+		}
+	}
 }
