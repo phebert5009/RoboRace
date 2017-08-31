fd1fed15e1b3b57bd3dffe009fe3a2a95b6910ee
= <cpierrot273@gmail.com>
2017-08-31 13:24:15 -0600

    added default font

--- src/robo_race/package.d
+++ src/robo_race/package.d
@@ -7,3 +7,15 @@ public import robo_race.tile;
 public import robo_race.mobile;
 public import robo_race.player;
 public import robo_race.hand;
+public import dsfml.graphics : Font;
+private Font defFont;
+struct 
+Font defaultFont() @property{
+    if(defFont) {
+        return defFont;
+    } else {
+        defFont = new Font();
+        if(!defFont.loadFromFile(".fonts/tuffy.ttf")) throw new Exception("could not load tuffy regular");
+        return defFont;
+    }
+}
